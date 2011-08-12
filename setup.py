9ddaf9f62cd2b29a6e8a48738ba35ec931ae375d
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-08-12 01:41:59 -0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name = "salesman",
+    version = "0.0.1",
+    description = "Automatic Link Checker",
+    author = "Kyle Conroy",
+    author_email = "kyle.j.conroy@gmail.com",
+    url = "http://github.com/derferman/salesman/",
+    keywords = ["http","checker"],
+    install_requires = ["requests", "lxml", "gevent"],
+    packages = find_packages(),
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = "Traveling Salesman")
